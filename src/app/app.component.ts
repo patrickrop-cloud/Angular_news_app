e8f56de393f373b0bbaccbf38b52176b7b654828
Patrick Rop <patrick.rop@student.moringaschool.com>
2022-10-13 23:58:32 +0300

    add styling and debug the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,37 @@ import { NewsService } from './services/news.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit,OnInit {
+export class AppComponent implements AfterViewInit {
 
   title = 'NewsApp';
-  public sources:any=[];
-  public articles:any=[];
-  public selectedNewsChannel:string="Top 10 Trending News!";
+  sources:any=[];
+  articles:any=[];
+  selectedNewsChannel:string="Top 10 Trending News!";
   @ViewChild(MatSidenav) sideNav! : MatSidenav;
  
-
-  constructor(
-    private Observer: BreakpointObserver,
-    private cdr: ChangeDetectorRef,
-    private newsApi: NewsService){
-
-  }
   ngOnInit(): void {
     this.newsApi.initArticles()
     .subscribe((res:any)=>{
       console.log(res);
       this.articles = res.articles;
-    });
+    })
     this.newsApi.initSources()
     .subscribe((res:any)=>{
       console.log(res);
       this.sources = res.sources;
     })
   }
+
+  constructor(
+    private observer: BreakpointObserver,
+    private cdr: ChangeDetectorRef,
+    private newsApi: NewsService){
+
+  }
+ 
   ngAfterViewInit(): void{
     this.sideNav.opened = true;
-    this.Observer.observe(['(max-width:787px)'])
+    this.observer.observe(['(max-width:787px)'])
     .subscribe((res)=>{
       if(res?.matches){
         this.sideNav.mode = "over";
@@ -51,11 +52,11 @@ export class AppComponent implements AfterViewInit,OnInit {
     this.cdr.detectChanges();
   }
   searchSource(source:any){
-    this.newsApi.getArticles(source)
-    .subscribe((res)=>{
+    this.newsApi.getArticlesByID(source.id)
+    .subscribe((res:any)=>{
       this.articles = this.articles;
       this.selectedNewsChannel = source.name
+      
     })
-
   }
 }
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -13,10 +13,11 @@ export class NewsService {
   initSources(){
     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
   }
-  getArticles(source: string){
-    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
-  }
   initArticles(){
     return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
   }
+  getArticlesByID(source: string){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+  
 }
